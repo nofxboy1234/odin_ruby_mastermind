d0ae568189268767bf15991358bdf27bbdf1374a
Dylan Palmboom <nofxboy1234@gmail.com>
2023-02-22 09:44:59 +0200

    Refactor class responsibilities

--- mastermind.rb
+++ mastermind.rb
@@ -1,69 +1,21 @@
 require 'pry-byebug'
 
 class CodeBreaker
-  attr_reader :board, :game
+  attr_reader :board
 
-  def initialize(board, game)
+  def initialize(board)
     @board = board
-    @game = game
   end
 
   def guess(code)
     place_pegs(code)
-    correct_guess?
   end
 
   private
 
   def place_pegs(code)
     # binding.pry
-    board.place_guess_pegs(code)
-  end
-
-  def correct_guess?
-    game.correct_guess?
-  end
-end
-
-class Board
-  attr_accessor :code_pegs
-
-  attr_reader :guess_pegs
-
-  def initialize
-    @guess_pegs = []
-  end
-
-  def place_guess_pegs(pegs)
-    guess_pegs << pegs
-  end
-end
-
-class Game
-  attr_reader :board
-  attr_accessor :maker, :breaker
-
-  def initialize
-    @board = Board.new
-    @maker = CodeMaker.new(board)
-    @maker.create_master_code('1234')
-    @breaker = CodeBreaker.new(board, self)
-    # @breaker.guess(1234)
-    game_loop
-  end
-
-  def game_loop
-    guess = nil
-
-    until guess == 'q'
-      puts 'Please enter your guess: '
-      guess = gets.chomp.strip
-      breaker.guess(guess)
-    end
-  end
-
-  def correct_guess?
-    puts board.guess_pegs.last == board.code_pegs
+    board.store_guess_pegs(code)
   end
 end
 
@@ -74,17 +26,68 @@ class CodeMaker
     @board = board
   end
 
-  def create_master_code(code)
+  def create_mastercode(code)
     place_pegs(code)
   end
 
   private
 
-  def place_pegs(code_pegs)
-    board.code_pegs = code_pegs
+  def place_pegs(code)
+    # binding.pry
+    board.store_code_pegs(code)
   end
 end
 
-# binding.pry
+class Board
+  attr_accessor :code_pegs
+  attr_reader :guess_pegs
 
-Game.new
+  def initialize
+    @code_pegs = nil
+    @guess_pegs = []
+  end
+
+  def store_guess_pegs(pegs)
+    guess_pegs << pegs
+  end
+
+  def store_code_pegs(pegs)
+    self.code_pegs = pegs
+  end
+end
+
+class Game
+  attr_reader :board
+  attr_accessor :maker, :breaker
+
+  def initialize(board, maker, breaker)
+    @board = board
+    @maker = maker
+    @breaker = breaker
+    game_loop
+  end
+
+  def game_loop
+    # binding.pry
+    maker.create_mastercode('1234')
+
+    guess = nil
+
+    until guess == 'q'
+      puts 'Please enter your guess: '
+      guess = gets.chomp.strip
+      breaker.guess(guess)
+      correct_guess?
+    end
+  end
+
+  def correct_guess?
+    puts board.guess_pegs.last == board.code_pegs
+  end
+end
+
+board = Board.new
+maker = CodeMaker.new(board)
+breaker = CodeBreaker.new(board)
+
+Game.new(board, maker, breaker)
