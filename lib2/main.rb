bf03706a7dc2fe869e4cda6754dd89b73f8f8db7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-10 12:40:27 +0200

    Refactor Main class

--- lib2/main.rb
+++ lib2/main.rb
@@ -19,7 +19,7 @@ require_relative 'code_breaker'
 
 # The Main class is responsible for the main flow of the game
 class Main
-  attr_reader :end_game, :main_menu, :game
+  attr_reader :end_game, :game, :maker, :breaker
 
   def initialize
     main_loop
@@ -29,9 +29,10 @@ class Main
 
   def main_loop
     until end_game
-      @main_menu = MainMenu.new(1, 3)
+      main_menu = MainMenu.new(1, 3)
 
-      run_menu_choice
+      run_menu_choice(main_menu)
+      run_game unless main_menu.choice.number == '3'
     end
     show_end_game_message
   end
@@ -40,23 +41,20 @@ class Main
     puts 'Thanks for playing, goodbye :)!'
   end
 
-  def run_menu_choice
+  def run_menu_choice(main_menu)
     case main_menu.choice.number
     when '1'
-      maker = CodeMaker.new(Computer.new)
-      breaker = CodeBreaker.new(Human.new)
+      @maker = CodeMaker.new(Computer.new)
+      @breaker = CodeBreaker.new(Human.new)
     when '2'
-      maker = CodeMaker.new(Human.new)
-      breaker = CodeBreaker.new(Computer.new)
+      @maker = CodeMaker.new(Human.new)
+      @breaker = CodeBreaker.new(Computer.new)
     when '3'
       @end_game = true
-      return
     end
-
-    run_game(maker, breaker)
   end
 
-  def run_game(maker, breaker)
+  def run_game
     @game = Game.new
     game.main_loop(maker, breaker)
   end
