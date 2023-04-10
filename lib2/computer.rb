05dc3573174349b84bd0c1f221f99be1fb9d572b
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-10 10:02:57 +0200

    Give Computer and Human functionality to guess and make mastercode
    Computer is guessing with random code. Need to change this
    to a guessing algorithm.

---
+++ lib2/computer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# The Computer class is responsible for representing a Computer and how
+# they create and guess a mastercode
+class Computer
+  def create_mastercode
+    random_code.join
+  end
+
+  def guess_mastercode
+    random_code.join
+  end
+
+  private
+
+  def random_code
+    (1..4).inject([]) { |random_numbers, _n| random_numbers << rand(1..6) }
+  end
+end
--- lib2/main.rb
+++ lib2/main.rb
@@ -13,13 +13,15 @@ require_relative 'clue_peg'
 require_relative 'clue_row'
 require_relative 'null_code_row'
 require_relative 'human'
+require_relative 'computer'
 
 # The Main class is responsible for the main flow of the game
 class Main
-  attr_reader :end_game, :main_menu, :game, :human
+  attr_reader :end_game, :main_menu, :game, :human, :computer
 
   def initialize
     @human = Human.new
+    @computer = Computer.new
 
     main_loop
   end
@@ -43,14 +45,18 @@ class Main
   def run_menu_choice
     case main_menu.choice.number
     when '1'
-      @game = Game.new(human, human)
-      game.main_loop
+      @game = Game.new(computer, human)
+      game_main_loop
     when '2'
-      @game = Game.new(human, human)
-      game.main_loop
+      @game = Game.new(human, computer)
+      game_main_loop
     when '3'
       @end_game = true
     end
+  end
+
+  def game_main_loop
+    game.main_loop
   end
 end
 
