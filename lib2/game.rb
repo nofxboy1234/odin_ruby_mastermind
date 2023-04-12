da17492477bb49eea5f8feeb9a81baad4e32c8b3
Dylan Palmboom <nofxboy1234@gmail.com>
2023-04-12 16:53:38 +0200

    Clean up commented code

--- lib2/game.rb
+++ lib2/game.rb
@@ -53,13 +53,13 @@ class Game
     board.store_code_row(code_row_menu.code)
   end
 
+  def matches_remaining?(number, secret_row_tally)
+    secret_row_tally[number].positive?
+  end
+
   def exact_match?(number, index, secret_row_tally)
     number == board.secret_row_numbers[index] &&
       matches_remaining?(number, secret_row_tally)
-  end
-
-  def matches_remaining?(number, secret_row_tally)
-    secret_row_tally[number].positive?
   end
 
   def partial_match?(number, index, secret_row_tally)
@@ -71,7 +71,6 @@ class Game
   end
 
   def calculate_clue(number, index, secret_row_tally)
-    # binding.pry
     if exact_match?(number, index, secret_row_tally)
       secret_row_tally[number] -= 1
       'x'
@@ -83,33 +82,15 @@ class Game
     end
   end
 
-  # def guess_peg_matches_remaining?(number, secret_row_tally)
-  #   secret_row_tally.any? do |secret_row_number, tally|
-  #     pegs_equal = number == secret_row_number
-  #     positive_tally = tally.positive?
-
-  #     pegs_equal && positive_tally
-  #   end
-  # end
-
-  # def numbers_index_with_matches_remaining(matches_remaining, last_code_row)
-  #   matches_remaining.each_with_index.filter_map do |match_left, index|
-  #     last_code_row.numbers_with_index[index] if match_left
-  #   end
-  # end
+  def clue
+    clue = last_code_row.numbers_with_index.map do |number, index|
+      calculate_clue(number, index, secret_row_tally)
+    end
+  end
 
   def clue_row
     last_code_row = board.code_rows.last
     secret_row_tally = board.secret_row_tally
-
-    # matches_remaining = last_code_row.numbers_with_index.map do |number, index|
-    #   guess_peg_matches_remaining?(number, secret_row_tally)
-    # end
-
-    # potential_matches = numbers_index_with_matches_remaining(matches_remaining, last_code_row)
-    clue = last_code_row.numbers_with_index.map do |number, index|
-      calculate_clue(number, index, secret_row_tally)
-    end
 
     ClueRow.new(clue)
   end
